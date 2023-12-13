b8503f66dcd71725cc7549665a5f25651885db36
Matteo Migliarini <matteo.migliarini.01@gmail.com>
2023-12-13 10:24:04 +0100

    Add load_dataset function

--- funcs/dataloader.py
+++ funcs/dataloader.py
@@ -136,3 +136,31 @@ class Datasets(Dataset):
         key, start_frame = self.data_idx[item]
         fs = np.arange(start_frame, start_frame + self.in_n + self.out_n)
         return self.p3d[key][fs]
+
+
+from utils import h36motion3d as datasets
+from torch.utils.data import DataLoader
+
+datas = 'h36m' # dataset name
+PATH = './data/h3.6m/h3.6m/dataset'
+# input_n=10 # number of frames to train on (default=10)
+# output_n=25 # number of frames to predict on
+# input_dim=3 # dimensions of the input coordinates(default=3)
+# skip_rate=1 # # skip rate of frames
+# joints_to_consider=22
+
+
+def load_dataset(input_n=10,output_n=25,skip_rate=1, batch_size=256):
+    '''Function that returns the two splits of the Human Motion dataset'''
+    print('Loading Train Dataset...')
+    dataset = datasets.Datasets(PATH,input_n,output_n,skip_rate, split=0)
+    print('Loading Validation Dataset...')
+    vald_dataset = datasets.Datasets(PATH,input_n,output_n,skip_rate, split=1)
+
+    print('>>> Training dataset length: {:d}'.format(dataset.__len__()))
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)#
+
+    print('>>> Validation dataset length: {:d}'.format(vald_dataset.__len__()))
+    vald_loader = DataLoader(vald_dataset, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
+
+    return data_loader, vald_loader
