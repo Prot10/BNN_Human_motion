0f3d297239c7c6ef84412b312e23a1fbc91d9b28
Matteo Migliarini <matteo.migliarini.01@gmail.com>
2023-12-16 13:37:36 +0100

    return also kl loss, nograd on CI functions

--- funcs/transformer/transformer.py
+++ funcs/transformer/transformer.py
@@ -13,7 +13,7 @@ output_n =25 # number of frames to predict on
 input_dim=3  # dimensions of the input coordinates(default=3)
 skip_rate=1  # skip rate of frames
 joints_to_consider=22
-lr = 1e-01 # learning rate
+lr = 1e-02 # learning rate
 use_scheduler=True # use MultiStepLR scheduler
 milestones=[5,10,15,20,25,30]   # the epochs after which the learning rate is adjusted by gamma
 gamma=0.1 #gamma correction to the learning rate, after reaching the milestone epochs
@@ -24,51 +24,53 @@ dim_used = np.array([6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24,
                     75, 76, 77, 78, 79, 80, 81, 82, 83, 87, 88, 89, 90, 91, 92])
 
 class LitSTTFormerBayes(L.LightningModule):
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, repetitions = 100, **kwargs) -> None:
         super().__init__()
-        self.transformer = STTFormerBayes(**kwargs)
-        self.repetitions = 1000
+        self.transformer = STTFormerBayes(repetitions = repetitions, **kwargs)
+        self.reps = repetitions
 
     def forward(self, x) -> Any:
         sequences_train = x[:, 0:input_n, dim_used].view(-1,input_n,len(dim_used)//3,3).permute(0,3,1,2)
         sequences_predict = self.transformer.forward(sequences_train).view(-1, output_n, joints_to_consider, 3)
         return sequences_predict
     
+    @torch.no_grad
     def mvn_fit(self, x : torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        y_samples = torch.stack([self.forward(x) for _ in range(self.repetitions)])
+        y_samples = torch.stack([self.forward(x) for _ in range(self.reps)])
         # sample is dimension 1000 x Batch x OutputFrames x Joints x 3
         mu = y_samples.mean(dim=0, keepdim=True)
         dev = y_samples - mu
-        S = torch.einsum('rbofi, rbofj -> bofij', dev, dev) / (self.repetitions-1)
+        S = torch.einsum('rbofi, rbofj -> bofij', dev, dev) / (self.reps-1)
         return mu, S
     
+    @torch.no_grad
     def build_ci(self, x: torch.Tensor, alpha:float=0.1, bonferroni:bool=True):
         if bonferroni:
             alpha = alpha/x.shape[-2]
             # correct for the number of joints
-        y_samples = torch.stack([self.forward(x) for _ in range(self.repetitions)])
+        y_samples = torch.stack([self.forward(x) for _ in range(self.reps)])
         # sample is dimension 1000 x Batch x OutputFrames x Joints x 3
         mu = y_samples.mean(dim=0, keepdim=True)
         diff = (y_samples - mu)
         dev = torch.linalg.vector_norm(diff, dim=-1)
-        return torch.quantile(dev, 1-alpha, dim=0)
+        dev = dev.numpy(force=True)
+        return mu.squeeze(), np.quantile(dev, 1-alpha, axis=0)
 
     def training_step(self, batch, batch_idx) -> STEP_OUTPUT:
-        loss = self.step(batch)
-        kl_loss = get_kl_loss(self.transformer)
+        loss, kl_loss = self.step(batch)
         self.log('training_loss', loss)
         self.log('KL_divergence', kl_loss)
-        return loss + kl_loss / batch.shape[0]
+        return loss + kl_loss / batch.shape[0] 
     
-    def step(self, batch) -> torch.Tensor:
+    def step(self, batch) -> Tuple[torch.Tensor, torch.Tensor]:
         batch = batch.float()
 
         sequences_train = batch[:, 0:input_n, dim_used].view(-1,input_n,len(dim_used)//3,3).permute(0,3,1,2)
         sequences_gt = batch[:, input_n:input_n+output_n, dim_used].view(-1,output_n,len(dim_used)//3,3)
 
-        sequences_predict = self.transformer.forward(sequences_train).view(-1, output_n, joints_to_consider, 3)
+        y_pred, kl_loss = self.transformer(sequences_train)
 
-        return mpjpe_error(sequences_predict,sequences_gt)
+        return mpjpe_error(y_pred,sequences_gt), kl_loss
 
 
     def configure_optimizers(self) -> OptimizerLRScheduler:
@@ -85,7 +87,7 @@ class LitSTTFormerBayes(L.LightningModule):
         return {'optimizer': optimizer, 'lr_scheduler': scheduler, 'monitor': 'val_loss'}
     
     def validation_step(self, batch, batch_idx):
-        loss = self.step(batch)
+        loss, _ = self.step(batch)
         self.log('validation_loss', loss)
 
 
