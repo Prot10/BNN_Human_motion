b18bbd865d103854da38d3ae682594b5f8132638
Matteo Migliarini <matteo.migliarini.01@gmail.com>
2023-12-21 19:01:22 +0100

    load testset as dictionary

--- funcs/dataloader.py
+++ funcs/dataloader.py
@@ -43,3 +43,14 @@ def load_test(actions:list,input_n=10, output_n=25, skip_rate=1, batch_size=256)
     test_dataset = datasets.Datasets(PATH, input_n, output_n, skip_rate, split=2, actions=actions)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=2, shuffle=False, pin_memory=True)
     return test_loader
+
+
+all_actions =  ["walking", "eating", "smoking", "discussion", "directions",
+               "greeting", "phoning", "posing", "purchases", "sitting",
+               "sittingdown", "takingphoto", "waiting", "walkingdog",
+               "walkingtogether"]
+def load_testset(input_n=10, output_n=25, skip_rate=1, batch_size=256):
+    actions = all_actions
+    ds = {action: datasets.Datasets(PATH, input_n, output_n, skip_rate, split=2, actions=[action]) for action in actions}
+
+    return {a: DataLoader(d, batch_size=batch_size, num_workers=2, shuffle=False, pin_memory=True) for a,d in ds.items()}
