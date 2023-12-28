9e2ad4b94ea76ec247b0c160bd7cbf64b85c598c
Matteo Migliarini <matteo.migliarini.01@gmail.com>
2023-12-28 19:41:44 +0100

    Add plots

--- funcs/__init__.py
+++ funcs/__init__.py
@@ -1,5 +1,5 @@
 from .autoformer import *
-from .dataloader import load_dataset
+from .dataloader import load_dataset, load_testset
 from .gated_recurrent_nn import *
 from .loss import *
 from .train import *
